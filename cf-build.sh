12648b3249ccc2100aea5059539da00192f0449b
Ben Coleman <benc.uk@gmail.com>
2025-12-18 12:58:46 +0000

    Build script for CF Pages

---
+++ cf-build.sh
@@ -0,0 +1,7 @@
+
+#!/bin/sh
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 10.0 -InstallDir ./dotnet
+./dotnet/dotnet --version
+make build-web
