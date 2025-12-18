6c6fc7bedbb04c1fb9c2a4c52fd16e33266e8629
Ben Coleman <benc.uk@gmail.com>
2025-12-18 13:00:01 +0000

    yes

--- cf-build.sh
+++ cf-build.sh
@@ -2,6 +2,8 @@
 #!/bin/sh
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
+
 ./dotnet-install.sh -c 10.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
+
 make build-web
